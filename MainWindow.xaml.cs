a572640c0c529cb052e17dd004bc76bc77d4041d
SW1pr0g <poryadin_alex@mail.ru>
2022-05-22 12:40:15 +0300

    main menu. start commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sea_battle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool exitt = false;
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {            
+            mainPanel.Visibility = Visibility.Hidden;
+            mainLabel.Visibility = Visibility.Visible;
+            exit_Mainbtn.Visibility = Visibility.Visible;
+        }
+        private void exit_Mainbtn_Click(object sender, RoutedEventArgs e)
+        {
+            exitt = true;
+            classes.system.exit exit = new classes.system.exit();
+            exit.exit_app();            
+        }       
+
+        private void main_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            mainPanel.Visibility = Visibility.Visible;
+            mainLabel.Visibility = Visibility.Hidden;
+            exit_Mainbtn.Visibility = Visibility.Hidden;
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {                      
+            classes.system.exit exit = new classes.system.exit();
+            e.Cancel = exit.exit_app_bool();        
+        }
+    }
+}
---
+++ classes/system/exit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace sea_battle.classes.system
+{
+    internal class exit
+    {
+        public bool exit_app_bool()
+        {            
+            bool res = false;
+            var response = MessageBox.Show("Вы действительно хотите выйти?", "Выход...",
+                                           MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+            if (response == MessageBoxResult.No)
+            {
+                res = true;
+            }
+            return res;
+        }
+        public void exit_app()
+        {
+            var response = MessageBox.Show("Вы действительно хотите выйти?", "Выход...",
+                                           MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+            if (response == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
